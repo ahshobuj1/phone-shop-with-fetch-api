12f2cb8fc045de1acbbf278e1c266fed65120aa0
A H Shobuj <ahshobuj09@gmail.com>
2024-07-07 16:54:35 +0600

    added show detail with modal card and almost complete the project

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //* Fetch data
 
-const fetchMobileData = async (search, showAll) => {
+const fetchMobileData = async (search) => {
     const response = await fetch(
         `https://openapi.programming-hero.com/api/phones?search=${search}`
     );
     const mobilesData = await response.json();
     const mobiles = mobilesData.data;
 
-    displayMobilesData(mobiles, showAll);
+    displayMobilesData(mobiles);
 };
 
 //* display mobiles data
 
-const displayMobilesData = (mobiles, showAll) => {
+const displayMobilesData = (mobiles) => {
     const parentContainer = document.getElementById('card_container');
     parentContainer.textContent = '';
 
@@ -24,12 +24,6 @@ const displayMobilesData = (mobiles, showAll) => {
         showAllButton.classList.remove('hidden');
     } else {
         showAllButton.classList.add('hidden');
-    }
-
-    //* added slice to debug data & handle show all
-
-    if (!showAll) {
-        mobiles = mobiles.slice(0, 12);
     }
 
     mobiles.map((mobile) => {
@@ -48,7 +42,7 @@ const displayMobilesData = (mobiles, showAll) => {
                             <p class="text-[#706F6F]">${mobile.slug}</p>
                             <h2 class="card-title">${mobile.brand}</h2>
                             <div class="card-actions">
-                                <button class="btn btn-primary">
+                                <button onclick="handleShowDetail('${mobile.slug}')" class="btn btn-primary">
                                     Show Details
                                 </button>
                             </div>
@@ -62,16 +56,12 @@ const displayMobilesData = (mobiles, showAll) => {
 
 //* search with Input value
 
-const searchWithInput = (showAll) => {
+const searchWithInput = () => {
     handleLoading(true);
-
     const searchInputElement = document.getElementById('searchText');
     const searchText = searchInputElement.value;
-
-    if (showAll) {
-        searchInputElement.value = '';
-    }
-    fetchMobileData(searchText, showAll);
+    searchInputElement.value = '';
+    fetchMobileData(searchText);
 };
 
 //* Set loading
@@ -109,8 +99,75 @@ const searchOppo = () => {
     handleSearchButton('search_oppo');
 };
 
-//* Handle show all
+//* Handle show details
 
-const handleShowAll = () => {
-    searchWithInput(true);
+const handleShowDetail = async (id) => {
+    //* Fetch data with id
+    const response = await fetch(
+        `https://openapi.programming-hero.com/api/phone/${id}`
+    );
+
+    const mobilesData = await response.json();
+    const data = mobilesData.data;
+    console.log(data);
+    showDetailModal(data);
+};
+
+//* show detail modal handle
+
+const showDetailModal = (data) => {
+    show_detail_modal.showModal();
+
+    /*    console.log(
+        data.mainFeatures.sensors.forEach((element) => {
+            console.log(element);
+        })
+    ); */
+
+    const parentModal = document.getElementById('modal_parent');
+
+    parentModal.textContent = '';
+    const createDiv = document.createElement('div');
+    createDiv.innerHTML = `
+    
+        <div class="card card-compact bg-base-100">
+                <figure><img src=${data.image} alt="phones" /></figure>
+                            
+                <h3 class="text-3xl font-bold my-6">${data.name}</h3>
+            <div class="space-y-2 px-0 mx-0">
+
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">Storage: </span>
+                ${data.mainFeatures.storage}</p>
+                                
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">Display size: </span>
+                ${data.mainFeatures.displaySize}</p>
+                                
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">Chipset: </span>
+                ${
+                    data.mainFeatures.chipSet
+                        ? data.mainFeatures.chipSet
+                        : 'no chip set available'
+                }</p>
+                                
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">Memory: </span>
+                ${data.mainFeatures.memory}</p>
+                                
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">Release date: </span>
+                ${data.releaseDate ? data.releaseDate : 'No date available'}</p>
+                                
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">Brand: </span>
+                ${data.brand}</p>
+                                
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">GPS: </span>
+                ${data.others?.GPS ? data.others.GPS : 'No GPS available'}</p>
+                                
+                <p class="text-lg text-[#706F6F]"><span class="text-xl font-semibold text-[#403F3F] px-0">Sensors: </span>
+                ${data.mainFeatures?.sensors}</p>
+                                
+                
+            </div>
+        </div>
+    
+    `;
+    parentModal.appendChild(createDiv);
 };
